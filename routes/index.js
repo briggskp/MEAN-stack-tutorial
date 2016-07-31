d07ea6ae8b2bd3946a0000a36611099e511f36ed
Monica Mow <mow.monica@gmail.com>
2016-07-31 15:23:51 -0700

    Add a route to read all to dos This get request should serve a json response containing an array of json objects which are all todos.

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res, next) {
 });
 
 /* CREATE to dos */
-router.post('/api/v1/todos', function(req,res) {
+router.post('/api/v1/todos', function(req, res) {
 
   var results = [];
 
@@ -40,10 +40,38 @@ router.post('/api/v1/todos', function(req,res) {
     query.on('end', function() {
       done();
       return res.json(results);
-    })
-  })
+    });
+  });
 });
 
 /* READ to dos */
+router.get('/api/v1/todos', function(req, res) {
+
+  var results = [];
+
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, function(err, client, done) {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+
+    // SQL Query > Select Data
+    var query = client.query("SELECT * FROM items ORDER BY id ASC");
+
+    // Stream results back one row at a time
+    query.on('row', function(row) {
+      results.push(row);
+    });
+
+    // After all data is returned, close connection and return results
+    query.on('end', function() {
+      done();
+      return res.json(results);
+    });
+  });
+});
 
 module.exports = router;
